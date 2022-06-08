14e709a50e5b933df81d454978930ac27f1dc999
fryn777 <febriansudiprasetya@gmail.com>
2022-06-08 22:14:12 +0700

    pull

--- Quiz Part 1 Week 2.sql
+++ Quiz Part 1 Week 2.sql
@@ -10,26 +10,14 @@ create table countries(
 	foreign key(region_id) references regions(region_id) 
 )
 
-drop table countries
-
 
 create table locations(
-    location_id character(2) primary key,
-    street_address varchar(50),
+    location_id serial primary key,
+    street_address varchar(40),
     postal_code varchar(12),
     city varchar(30) not null,
     state_province varchar(25),
-    country_id varchar(2),
-    foreign key(country_id) references countries(country_id) 
-)
-
-create table locations(
-    location_id character(2) primary key,
-    street_address varchar(50),
-    postal_code varchar(12),
-    city varchar(30) not null,
-    state_province varchar(25),
-    country_id varchar(2),
+    country_id varchar(2) not null,
     foreign key(country_id) references countries(country_id) 
 )
 
@@ -38,7 +26,7 @@ create table departments(
 
     departments_id serial primary key,
     departments_name varchar (30) not null,
-    location_id character(2),
+    location_id serial,
     foreign key(location_id) references locations(location_id)
     
 )
@@ -67,6 +55,8 @@ create table employees(
     foreign key(departments_id) references departments(departments_id)
 )
 
+alter table employees add CONSTRAINT fk_job_employee foreign key (job_id) references jobs (job_id)
+
 
 create table dependents(
 
@@ -80,6 +70,7 @@ create table dependents(
 )
 
 
+
 create table project_assignment(
 
     pras_proj_id integer,
@@ -90,11 +81,7 @@ create table project_assignment(
     
 )
 
-
-select * from project_assignment;
-
 ALTER TABLE ONLY project_assignment ADD CONSTRAINT "ID_PKEY" PRIMARY KEY (pras_proj_id,pras_employee_id);
-
 
 create table projects(
 
@@ -105,44 +92,55 @@ create table projects(
     proj_duedate date,
     proj_cust_name varchar(100)
     
-   
 )
 
-//no1
-create table project_employess(
+///No1
+create table emplo_proj(
     
     employee_id integer,
     project_id integer,
     name_employess varchar(20),
     project_employes varchar(20),
-    CONSTRAINT project_employess PRIMARY KEY (employee_id, project_id),
-    CONSTRAINT fk_project_employess foreign key (employee_id) references projects (employee_id),
-    CONSTRAINT fk_project_employess foreign key (project_id) references projects (project_id)
-   
+    CONSTRAINT employees_project PRIMARY KEY (employee_id, project_id)
+
 )
 
-//no2
-alter table regions add column proj_description varchar(255) 
-alter table regions add column proj_status varchar(15) 
-alter table regions add column proj_amount numeric  
 
-//no3
-alter table regions add column proj_account_mgr integer
-CONSTRAINT fk_project_employess foreign key (employee_id) references projects (employee_id),
-CONSTRAINT fk_project_employess foreign key (project_id) references projects (project_id)
+alter table emplo_proj add CONSTRAINT fk_project_employee foreign key (employee_id) references employees (employee_id)
+alter table emplo_proj add CONSTRAINT fk_employee_project foreign key (project_id) references projects (proj_id)
 
-//no4
-INSERT INTO projects(proj_id,proj_name,proj_createdon,proj_duedate,proj_cust_name) VALUES ('1','E-COMMERCE',now(),now()+interval '3 day','TELKOMSEL') 
-INSERT INTO projects(proj_id,proj_name,proj_createdon,proj_duedate,proj_cust_name) VALUES ('2','TRAVELOKA',now(),now()+interval '4 day','XL')
-INSERT INTO projects(proj_id,proj_name,proj_createdon,proj_duedate,proj_cust_name) VALUES ('3','TIKET.COM',now(),now()+interval '5 day','INDOSAT')
+///No2
+alter table projects add column proj_description varchar(255) 
+alter table projects add column proj_status varchar(15) 
+alter table projects add column proj_amount numeric  
 
-//no5
-INSERT INTO projects_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras,enddate,pras_status) VALUES ('1','1',now(),'','ASSIGNED') 
-INSERT INTO projects_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras,enddate,pras_status) VALUES ('2','1',now(),now()+interval '3 day','COMPLETED') 
-INSERT INTO projects_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras,enddate,pras_status) VALUES ('3','1',now(),'','INPROGRESS') 
-INSERT INTO projects_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras,enddate,pras_status) VALUES ('4','2',now(),'','ASSIGNED') 
-INSERT INTO projects_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras,enddate,pras_status) VALUES ('5','2',now(),now()+interval '3 day','COMPLETED') 
-INSERT INTO projects_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras,enddate,pras_status) VALUES ('6','2',now(),now()+interval '3 day','INPROGRESS') 
+///No3
+alter table projects add column proj_account_mgr integer
+alter table project_assignment ADD CONSTRAINT fk_pras_proj_id FOREIGN KEY (pras_proj_id) REFERENCES projects (proj_id) ON UPDATE CASCADE ON DELETE CASCADE
+alter table project_assignment ADD CONSTRAINT fk_pras_employee_id FOREIGN KEY (pras_employee_id) REFERENCES employees (employee_id) ON UPDATE CASCADE ON DELETE CASCADE
 
 
+///No4
+INSERT INTO projects(proj_id,proj_name,proj_createdon,proj_duedate,proj_cust_name,proj_status) VALUES ('1','E-COMMERCE',now(),now()+interval '3 day','TELKOMSEL','INPROGRESS')
+INSERT INTO projects(proj_id,proj_name,proj_createdon,proj_duedate,proj_cust_name,proj_status) VALUES ('2','TRAVELOKA',now(),now()+interval '3 day','XL','INPROGRESS')
+INSERT INTO projects(proj_id,proj_name,proj_createdon,proj_duedate,proj_cust_name,proj_status) VALUES ('3','TIKET.COM',now(),now()+interval '3 day','INDOSAT','INPROGRESS')
 
+
+///No5
+INSERT INTO project_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras_enddate,pras_status)
+SELECT projects.proj_id,employees.employee_id,projects.proj_createdon,projects.proj_duedate,projects.proj_status
+FROM projects,employees
+WHERE employees.department_id = 6 
+
+//IniBuatmanualinsertnya
+INSERT INTO project_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras_status) VALUES ('1','104',now(),'ASSIGNED')
+INSERT INTO project_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras_enddate,pras_status) VALUES ('1','105',now(),now()+interval '3 day','COMPLETED');
+INSERT INTO project_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras_status) VALUES ('1','106',now(),'INPROGRESS');
+INSERT INTO project_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras_status) VALUES ('2','104',now(),'ASSIGNED');
+INSERT INTO project_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras_enddate,pras_status) VALUES ('2','105',now(),now()+interval '3 day','COMPLETED');
+INSERT INTO project_assignment(pras_proj_id,pras_employee_id,pras_startdate,pras_enddate,pras_status) VALUES ('2','106',now(),now()+interval '3 day','INPROGRESS');
+
+
+
+//No6
+SELECT * FROM project_assignment WHERE pras_status = 'COMPLETED'
